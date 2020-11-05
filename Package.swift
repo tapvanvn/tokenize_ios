ffe6930d4264d8bede8ff6a4db3a3395f4250646
Duy Nguyen <tapvanvn@gmail.com>
2020-11-05 16:01:00 +0700

    update package name

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "tokenize_ios",
+    name: "Tokenize",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "tokenize_ios",
-            targets: ["tokenize_ios"]),
+            name: "Tokenize",
+            targets: ["Tokenize"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "tokenize_ios",
+            name: "Tokenize",
             dependencies: []),
         .testTarget(
             name: "tokenize_iosTests",
-            dependencies: ["tokenize_ios"]),
+            dependencies: ["Tokenize"]),
     ]
 )
