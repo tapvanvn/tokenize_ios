89388f21aeed015402d34454da6588ecb9aec17d
Duy Nguyen <tapvanvn@gmail.com>
2020-11-10 11:04:15 +0700

    remove unuse code

--- Sources/Tokenize/pattern.swift
+++ Sources/Tokenize/pattern.swift
@@ -5,16 +5,14 @@ public class Pattern {
 	
 	public var type : Int
 	public var content : String
-	public var is_phrase_until : Bool
 	public var is_ignore_in_result : Bool
 	public var can_nested : Bool
 	public var export_type : Int
 
-	public init(type: Int, content: String, is_phrase_until: Bool, is_ignore_in_result: Bool, can_nested: Bool, export_type: Int) {
+	public init(type: Int, content: String, is_ignore_in_result: Bool, can_nested: Bool, export_type: Int) {
 
 		self.type = type 
 		self.content = content
-		self.is_phrase_until = is_phrase_until
 		self.is_ignore_in_result = is_ignore_in_result 
 		self.can_nested = can_nested 
 		self.export_type = export_type
@@ -24,7 +22,6 @@ public class Pattern {
     public convenience init(type: Int) {
 		self.init(type: type, 
 			content: "", 
-			is_phrase_until: false, 
 			is_ignore_in_result: false, 
 			can_nested: false, 
 			export_type: 0)
@@ -35,7 +32,6 @@ public class Pattern {
         
         self.init(type: 0,
             content: content,
-            is_phrase_until: false,
             is_ignore_in_result: false,
             can_nested: false,
             export_type: 0)
@@ -45,7 +41,6 @@ public class Pattern {
         
 		self.init(type: type, 
 			content: "", 
-			is_phrase_until: false, 
 			is_ignore_in_result: false, 
 			can_nested: can_nested, 
 			export_type: 0)
@@ -55,20 +50,20 @@ public class Pattern {
 
 		self.init(type: type, 
 			content: "", 
-			is_phrase_until: false, 
 			is_ignore_in_result: is_ignore_in_result, 
 			can_nested: false, 
 			export_type: 0)
 	}
 
-    public convenience init(export_type: Int, is_phrase_until: Bool) {
-		self.init(type: 0, 
-			content: "", 
-			is_phrase_until: is_phrase_until, 
-			is_ignore_in_result: false, 
-			can_nested: false, 
-			export_type: export_type)
-	}
+	//pattern define a token_type
+    public convenience init(content: String, is_ignore_in_result: Bool) {
+        
+        self.init(type: 0,
+            content: content,
+            is_ignore_in_result: is_ignore_in_result,
+            can_nested: false,
+            export_type: 0)
+    }
 }
 
 //Pattern define a pattern is a array of token type
